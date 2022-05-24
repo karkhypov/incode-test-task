8c694fd617dad23fc3a00e4678628aa39cb9ed33
Kirill Arkhypov <k.arkhypov@gmail.com>
2022-05-24 14:47:06 +0300

    persistent pause bug fixed

--- server/server.js
+++ server/server.js
@@ -87,10 +87,10 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-let interval;
-let paused = false;
-
 socketServer.on('connection', (socket) => {
+  let interval;
+  let paused = false;
+
   getInitialQuotes(socket);
 
   socket.on('start', () => {
